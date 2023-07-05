ceb4b024e955f157ab816318394c345ec62be9e5
Sunil <suneel.kms123@gmail.com>
2023-07-05 12:53:57 +0530

    Update UserProfileEntity.java

--- src/main/java/com/uk/sprint/rest/infrastructure/UserProfileEntity.java
+++ src/main/java/com/uk/sprint/rest/infrastructure/UserProfileEntity.java
@@ -1,13 +1,13 @@
 package com.uk.sprint.rest.infrastructure;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
 @Entity
